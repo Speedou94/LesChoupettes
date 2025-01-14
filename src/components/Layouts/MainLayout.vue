0417aca66dde9f8cf87d4831d821f7fc182bb632
Speedou <j.azran23@gmail.com>
2025-01-14 14:32:42 +0100

    Ajout ecran d'accueil du site

--- src/components/Layouts/MainLayout.vue
+++ src/components/Layouts/MainLayout.vue
@@ -1,7 +1,11 @@
 <template>
   <div>
     <NavBar />
-    <router-view></router-view>
+    <Home />
+    <About />
+    <Gameplay />
+    <Manifest />
+    <Contact />
     <Footer />
   </div>
 </template>
@@ -9,12 +13,22 @@
 <script>
 import NavBar from "@/components/Utils/NavBar";
 import Footer from "@/components/Utils/Footer";
+import Home from "@/components/Views/Home";
+import About from "@/components/Views/About";
+import Gameplay from "@/components/Views/Gameplay";
+import Manifest from "@/components/Views/Manifest";
+import Contact from "@/components/Views/Contact";
 
 export default {
   name: "MainLayout",
   components: {
     NavBar,
-    Footer
+    Home,
+    About,
+    Gameplay,
+    Manifest,
+    Contact,
+    Footer,
   }
 };
 </script>
