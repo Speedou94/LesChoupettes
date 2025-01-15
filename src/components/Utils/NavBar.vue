fb9e3c046a7a09c038b8b1f81831c626cb2c070e
Speedou <j.azran23@gmail.com>
2025-01-15 19:04:30 +0100

    Mise à jour du bouton burger en blanc et responsive

--- src/components/Utils/NavBar.vue
+++ src/components/Utils/NavBar.vue
@@ -5,7 +5,7 @@
       aria-label="Toggle navigation">
       <span class="navbar-toggler-icon"></span>
     </button>
-    <div class="navbar-collapse collapse" id="navbarNavAltMarkup">
+    <div class="navbar-collapse collapse justify-content-center" id="navbarNavAltMarkup">
       <ul class="navbar-nav ms-auto navbar-nav-scroll">
 
         <li class="nav-item">
@@ -40,16 +40,25 @@ export default {
 /* Ajoutez des styles pour l'état ouvert du menu burger */
 .offcanvas-collapse.open {
   display: block;
+  /* Affiche le menu */
   visibility: visible;
+  /* Rend le menu visible */
   transform: translateX(0);
+  /* Positionne le menu à sa place d'origine */
 }
 
 .navbar {
+
   background-color: rgba(255, 255, 255, 0.1);
   /* Couleur de fond semi-transparente */
   padding: 1rem;
   /* Ajoute un padding pour le contenu */
   backdrop-filter: blur(10px);
   /* Applique un flou à l'arrière-plan */
+}
+
+.navbar-toggler-icon {
+  background-image: url("data:image/svg+xml;charset=utf8,%3Csvg viewBox='0 0 30 30' xmlns='http://www.w3.org/2000/svg'%3E%3Cpath stroke='rgba%28255, 255, 255, 0.5%29' stroke-width='2' stroke-linecap='round' stroke-miterlimit='10' d='M4 7h22M4 15h22M4 23h22'/%3E%3C/svg%3E");
+  /* Utilise une icône de burger par défaut */
 }
 </style>
