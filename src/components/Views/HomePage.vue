acc81cac61de6b96eb091fa81f0617485a2ab8ae
Speedou <j.azran23@gmail.com>
2025-06-11 23:29:30 +0200

    test3

--- src/components/Views/HomePage.vue
+++ src/components/Views/HomePage.vue
@@ -1,18 +1,21 @@
 <template>
   <div class="home-page">
     <div class="content">
-      <img :src="`${process.env.BASE_URL}assets/images/LCSLogo.webp`" alt="LCS Logo" class="logo">
-
-      <img :src="`${process.env.BASE_URL}assets/images/LCSLogo.webp`" alt="LCS Logo" class="logo">
+      <img :src="logoUrl" alt="LCS Logo" class="logo">
+      <img :src="logoUrl" alt="LCS Logo" class="logo">
       <button class="btn btn-primary rounded-pill" @click="enterSite">Entrer sur le site</button>
     </div>
-
   </div>
 </template>
 
 <script>
 export default {
   name: "HomePage",
+  data() {
+    return {
+      logoUrl: `${process.env.BASE_URL}assets/images/LCSLogo.webp`
+    };
+  },
   methods: {
     enterSite() {
       this.$router.push('/home');
@@ -32,18 +35,6 @@ export default {
 }
 
 .content {
-  display: flex;
-  position: absolute;
-  top: 50%;
-  left: 50%;
-  transform: translate(-50%, -50%);
-  text-align: center;
-  flex-direction: column;
-  align-content: center;
-  justify-content: center;
-}
-
-.content2 {
   display: flex;
   position: absolute;
   top: 50%;
